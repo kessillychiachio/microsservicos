b20183d67de8c330aa24666738fc20a2e78789c7
Ana Chiachio <kessilly_chiachio@hotmail.com>
2025-04-08 15:18:19 -0300

    add contraindicacoes

--- recomendacoes/app.py
+++ recomendacoes/app.py
@@ -2,4 +2,15 @@ from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-sugestoes = 
+sugestoes = {
+  "ansiedade": ["Lavanda", "Camomila", "Bergamota"],
+  "insônia": ["Lavanda", "Manjerona", "Cedro"],
+  "estresse": ["Hortelã-pimenta", "Ylang Ylang", "Laranja-selvagem"]
+}
+
+@app.post("/recomendar"):
+def recomendar(sintomas: list[str] = Body(...)):
+    resultado = set()
+    for sintoma in sintomas:
+      resultado.update(sugestoes.get(sintoma.lower(), []))
+    return {"recomendados": list(resultado)}
