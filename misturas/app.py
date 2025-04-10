2fc425727ea06b7c680c1dc1674a4ad333db4d8a
Ana Chiachio <kessilly_chiachio@hotmail.com>
2025-04-10 12:43:15 -0300

    correcao cliente

--- misturas/app.py
+++ misturas/app.py
@@ -17,15 +17,15 @@ class Mistura (BaseModel):
  oleos: list[str]
  
 @app.post("/misturas")
-def criar_mistura(mistura:Mistura):
-  for a1, a2 in antagonicos:
-    if a1 in mistura.oleos and a2 in mistura.oleos:
-      raise HTTPException(
-        status_code=400,
-        detail=f"Não é permitido combinar '{a1}' com '{a2}' na mesma mistura, pois os seus efeitos são inversos e não agregam juntos. Escolha outros óleos."
-      )
-  misturas_salvas.append(mistura)
-  return {"mensagem":"Mistura salva com sucesso"}
+def criar_mistura(mistura: Mistura):
+    for a1, a2 in antagonicos:
+        if a1 in mistura.oleos and a2 in mistura.oleos:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Não é permitido combinar '{a1}' com '{a2}' na mesma mistura, pois os seus efeitos são inversos e não agregam juntos. Escolha outros óleos."
+            )
+    misturas_salvas.append(mistura)
+    return {"mensagem": "Mistura salva com sucesso", "mistura": mistura}
 
 @app.get("/misturas")
 def listar_misturas():
